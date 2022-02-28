21433c9e01714d1ad362f9fe0b0268d448f75f1e
xmg <2474775945@qq.com>
2022-02-28 10:19:55 +0800

    实现下划线滑动

--- xm-ui-1/src/components/Tabsdemo.vue
+++ xm-ui-1/src/components/Tabsdemo.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-      玩的tabs组件
+    <Tabs v-model:selected='x'>    
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容2</Tab>
+    </Tabs>
+  
   </div>
 </template>
 
 <script>
+import Tabs from "../lib/Tabs.vue"
+import Tab from "../lib/Tab.vue"
+import { ref } from 'vue'
 export default {
-
+  components:{
+    Tabs,Tab
+  },
+  setup(){
+    const x = ref('导航1')
+    return {x}
+  }
 }
 </script>
 
